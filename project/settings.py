8b98d0745f98aca34cbf21f187ed6d92efa0ea56
Surya9122prakash <100427401+Surya9122prakash@users.noreply.github.com>
2023-06-06 22:29:22 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
+from django-environ import environ
 env = environ.Env()
 
 environ.Env.read_env()
